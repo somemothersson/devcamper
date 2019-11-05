da2814588379a2f25c9aeb364ce25f35abf71024
Stephen Malito <stevemalito@gmail.com>
2019-11-04 21:21:41 -0600

    basics of req-res

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
-const http = require('http')
+const http = require("http");
 
+const todos = [
+  { id: 1, text: "Todo one" },
+  { id: 2, text: "Todo two" },
+  { id: 3, text: "Todo three" }
+];
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text-plain')
-    res.write("Hello")
-    res.end()
+  res.writeHead(404, {
+    "Content-Type": "application/json",
+    "X-Powered-By": "MY BUTTHOLE"
+  });
 
+//   console.log(req.headers.authorization);
+
+let body = []
+  req.on('data', chunk => {
+    body.push(chunk)
+  }).on('end', () => {
+      body = Buffer.concat(body).toString()
+      console.log(body)
+  })
+
+  res.end(
+    JSON.stringify({
+      success: true,
+      data: todos,
+    
+    })
+  );
 });
 
-const PORT = 8080
+const PORT = 8080;
 
-server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
+server.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
